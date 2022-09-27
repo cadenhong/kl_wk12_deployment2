16e64dcb3fba7d8605cb95039e674516f7c2cc1f
Caden Hong <caden.p.hong@gmail.com>
2022-09-27 12:15:19 -0400

    updating ebcli setup code

--- setup_ebcli.sh
+++ setup_ebcli.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-# run as a jenkins user on EC2
+# Script to install and set up EB CLI
+# For Deployment 2 - run this on the EC2 as jenkins user
+
+echo "Downloading EB CLI..."
+sleep 2
+
 pip install awsebcli --upgrade --user
+
+sleep 1
+echo "Exporting PATH variable to include Jenkins bin location..."
+
+export PATH="/var/lib/Jenkins/.local/bin:$PATH"
+
+sleep 1
+echo "Checking EB version..."
+sleep 1
+
 eb --version
